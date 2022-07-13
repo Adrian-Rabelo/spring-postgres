459a3d740b8453891e5b414dd16913c7e0657eba
Ádrian Rabelo <adrianrabeloe10@gmail.com>
2022-07-13 08:55:02 -0400

    backend

---
+++ src/main/java/com/example/vuespring/repository/TutorialRepository.java
@@ -0,0 +1,11 @@
+package com.example.vuespring.repository;
+
+import com.example.vuespring.model.Tutorial;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface TutorialRepository extends JpaRepository<Tutorial, Long> {
+    List<Tutorial> findByPublished(boolean published);
+    List<Tutorial> findByTitleContaining(String title);
+}
